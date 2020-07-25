5e48da093f3446e4ece42c13e13456f974165f30
anyemelody <anyemelody@gmail.com>
2020-07-25 13:40:49 -0400

    setup boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    filename: "js/main.js",
+    //the output folder should be the entire folder
+    //so it will update the js and html files
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      templete: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
